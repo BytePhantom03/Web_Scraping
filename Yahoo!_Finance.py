be6a6e10766cf258662a751906cdc7efe428b52b
Ravi Zapadiya <167902767+BytePhantom03@users.noreply.github.com>
2025-06-07 08:46:34 +0530

    Update Yahoo!_Finance.py

--- Yahoo!_Finance.py
+++ Yahoo!_Finance.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[35]:
-
-
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
@@ -12,9 +6,6 @@ from selenium.webdriver.common.action_chains import ActionChains
 import time
 import pandas as pd
 import numpy as np
-
-
-# In[5]:
 
 
 driver = webdriver.Chrome()
@@ -34,18 +25,7 @@ def wait_for_page_load(driver,wait):
     else:
         print(f"Page \"{page_title}\" is Loaded Sucessfully")
 
-
-# def wait_for_page_load(driver, wait, old_title=None):
-#     try:
-#         wait.until(lambda d: d.execute_script("return document.readyState") == "complete")
-#         if old_title:
-#             wait.until(lambda d: d.title != old_title)
-#     except:
-#         print(f"Page is \"{old_title}\" not loaded in given time")
-#     else:
-#         print(f"Page \"{driver.title}\" is Loaded Successfully")
-
-    
+   
 
 url = "https://finance.yahoo.com/"
 driver.get(url)
@@ -64,8 +44,6 @@ actions.move_to_element(market_menu).perform()
 # click On Trending ticker
 
 trending_ticker = wait.until(
-    # EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/header/div/div/div/div[4]/div/div/ul/li[3]/div/ul/li[4]/a/div'))
-     # EC.element_to_be_clickable((By.XPATH, '//a[contains(@href,"//*[@id="ybar-navigation"]/div/ul/li[3]/div/ul/li[4]/a/div")]'))
     EC.element_to_be_clickable(
     (By.XPATH, '//a[contains(@href,"trending-tickers")]')
 )
@@ -78,7 +56,6 @@ wait_for_page_load(driver,wait)
 
 most_active = wait.until(
     EC.presence_of_element_located((By.XPATH , '/html/body/div[2]/main/section/section/section/article/section[1]/div/nav/ul/li[1]/a'))
-    # EC.element_to_be_clickable((By.XPATH, '//a[contains(@href,"most-active")]'))
 )
 most_active.click()
 wait_for_page_load(driver,wait)
@@ -110,13 +87,10 @@ while True:
 
     # checking Clickable
 
-
-
     # click next page
 
     try:
         next_button = wait.until(
-            # EC.presence_of_element_located((By.XPATH,"/html/body/div[2]/main/section/section/section/article/section[1]/div/div[3]/div[3]/button[3]/div/*[name()='svg'][1]"))
             EC.presence_of_element_located((By.XPATH,'//*[@id="nimbus-app"]/section/section/section/article/section[1]/div/div[3]/div[3]/button[3]'))
         )
 
@@ -132,22 +106,6 @@ while True:
         time.sleep(2)
 
 
-
-    
-
-
-
-
-
-
-
-
-
-
-
-# In[38]:
-
-
 stoke_table = pd.DataFrame(data).apply(lambda col:col.str.strip() if col.dtype == "object" else col).assign(
     price = lambda df_: pd.to_numeric(df_.price),
     change = lambda df_: pd.to_numeric(df_.change.str.replace('+','')),
@@ -161,37 +119,14 @@ stoke_table = pd.DataFrame(data).apply(lambda col:col.str.strip() if col.dtype =
 })
 
 
-# In[39]:
-
 
 stoke_table
 
 
-# In[40]:
-
-
-stoke_table.dtypes
-
-
-# In[43]:
-
-
 get_ipython().system('pip install openpyxl')
-
-
-# In[44]:
-
 
 stoke_table.to_excel("Yahoo-Stoke-data.xlsx", index=False) 
 
-
-# In[1]:
-
-
-
-
-
-# In[ ]:
 
 
 
